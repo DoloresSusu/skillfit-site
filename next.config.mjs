a921b2fecc74ebacc2b64db8d7af319af4d35a3a
DoloresSusu <sumengxin@gmail.com>
2026-04-25 02:41:34 +0800

    Redirect www to apex domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  poweredByHeader: false
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.get-skill-fit.com" }],
+        destination: "https://get-skill-fit.com/:path*",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
-
